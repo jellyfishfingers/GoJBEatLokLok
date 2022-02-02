1d38098e38a6b920bc9400b58ac010c12ad93bbf
Jiayuan <lian.jiayuan@gmail.com>
2022-02-03 00:50:48 +0800

    Added request for Tuas (2nd Causeway).

--- js/request.js
+++ js/request.js
@@ -9,6 +9,7 @@ var currentdateJS = dayjs();
 
 goJBEatLokLok(getURL());
 beforeCovid(currentdate, getURL());
+Jurong(getURL());
 
 function dateDisplay(date)
 {
@@ -38,7 +39,7 @@ function goJBEatLokLok(requestURL) {
           if (data.items[0].cameras[key].camera_id === "2701"){
               image = data.items[0].cameras[key].image;
             
-              $(".currentWoodlandsCausewayTraffic").append("<img src='" + image + "' width='640px'>");
+              $(".currentWoodlandsCausewayTraffic").append("<img class='displaypic' src='" + image + "' width='640px'>");
           }    
       });
 
@@ -61,8 +62,7 @@ function beforeCovid(requestDate, requestURL) {
     parameter = parameter + dateNumbersToString(requestDate.getHours()) + "%3A" + dateNumbersToString(requestDate.getMinutes()) + "%3A" + dateNumbersToString(requestDate.getSeconds()); 
 
     requestURL = requestURL + parameter;    
-    //"https://api.data.gov.sg/v1/transport/traffic-images?date_time=2020-10-05T10%3A05%3A05"
-    debugger;
+ 
 
     $.getJSON(requestURL , function(data) { 
         console.log(data.items[0].cameras[12].camera_id);
@@ -74,11 +74,30 @@ function beforeCovid(requestDate, requestURL) {
             if (data.items[0].cameras[key].camera_id === "2701"){
                 image = data.items[0].cameras[key].image;
               
-                $(".beforeCovid19").append("<img src='" + image + "' width='640px'>");
+                $(".beforeCovid19").append("<img class='displaypic' src='" + image + "' width='640px'>");
             }    
         });
 
     });
 }
+
+function Jurong(requestURL) {
+
+  requestURL = requestURL;    
+
+  $.getJSON(requestURL , function(data) { 
+      $.each(data.items[0].cameras, function (key,value){
+          //Get only Camera 4703's Data: Tuas-Causeway Camera.
+          if (data.items[0].cameras[key].camera_id === "4703"){
+              image = data.items[0].cameras[key].image;
+            
+              $(".currentJurongCausewayTraffic").append("<img class='displaypic' src='" + image + "' width='640px'>");
+          }    
+      });
+
+  });
+}
+
+
 
 });
